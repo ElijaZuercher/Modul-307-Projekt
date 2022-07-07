af963afca3692a1da7a11d9cd455f57ee27ce3eb
Tschawi <89629023+Tschawi@users.noreply.github.com>
2022-07-07 16:26:08 +0200

    Fertig donnerstag ük

--- _Framework/app/Controllers/HomeController.php
+++ _Framework/app/Controllers/HomeController.php
@@ -16,15 +16,23 @@ class HomeController
 		if($_SERVER['REQUEST_METHOD']==='POST') {
 			$pdo = db();
 			$statement = $pdo->prepare('INSERT INTO ausleihen (name, email, telefon, mitgliedschaft, ausgeleihtesvideo) VALUES (:name, :email, :telefon, :mitgliedschaft, :film);');
-			$statement->bindParam(':name', $_POST['name']);
-			$statement->bindParam(':email', $_POST['email']);
-			$statement->bindParam(':telefon', $_POST['telefon']);
-			$statement->bindParam(':mitgliedschaft', $_POST['mitgliedschaft']);
-			$statement->bindParam(':film', $_POST['film']);
-			$statement->execute();
+			if($_POST['name'] == '' ) {
+				echo "<script type='text/javascript'>window.alert('Bitte einen Namen eingeben'); window.location.href='home';</script>";
+			} elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+				echo "<script type='text/javascript'>window.alert('Bitte eine gültige E-Mailadresse eingeben'); window.location.href='home';</script>";
+			} elseif (!preg_match('/^[\+ 0-9]+$/', $_POST['telefon'])){
+				echo "<script type='text/javascript'>window.alert('Bitte eine gültige Telefonnummer eingeben'); window.location.href='home';</script>";
+			} else {
+				$statement->bindParam(':name', $_POST['name']);
+				$statement->bindParam(':email', $_POST['email']);
+				$statement->bindParam(':telefon', $_POST['telefon']);
+				$statement->bindParam(':mitgliedschaft', $_POST['mitgliedschaft']);
+				$statement->bindParam(':film', $_POST['film']);
+				$statement->execute();
+				header("Location: home");
+			}
 		}
 		// Umleiten auf die Startseite
-		header("Location: home");
 	}
 	
 }
