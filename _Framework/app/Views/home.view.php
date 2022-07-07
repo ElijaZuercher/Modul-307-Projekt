9dd63ea2f55600eb903cdefd50eaa7c317408c91
Tschawi <89629023+Tschawi@users.noreply.github.com>
2022-07-07 15:26:37 +0200

    DEsign

--- _Framework/app/Views/home.view.php
+++ _Framework/app/Views/home.view.php
@@ -11,81 +11,54 @@
     <div class="top">
 
         <div class="topg"><a href="#home" class="Home">Home</a></div>
-        <div class="topg"><a href="#filme" class="Filme">Filme</a></div>
-        <div class="topg"><a href="#Kunden" class="Kunden">Kunden</a></div>
 
     </div>
 
-    <div class="AusleiheundErfassen">
-        <div class="Ausleihe">
-            <div class="Ausleihe-übercontainer">
-                <div class="Ausleihe-Text">Ausleihen</div>
+    <div class="container">
+        <div class="andere">
+            <div class="Textüberschrifft">
+                <div class="textitem" >Id</div>
+                <div class="textitem">Kunde</div>
+                <div class="textitem">Status</div>
+                <div class="textitem">Film</div>
             </div>
-    
-            <div class="Ausleihe-Box">
-                <div class="Box-id">Id
-                    <?php 
+            <div class="ausgabesql">
+                <?php 
                         foreach ($result as $ausleihe){
-        	                echo($ausleihe['id']);
+        	                echo("<div class='space'>"."<p class='pid'>".$ausleihe['id']."</p><p class='pname'>".$ausleihe['name']."</p><p class='pstatus'>".$ausleihe['ausleihstatus']."</p><p class='pfilm'>".$ausleihe['ausgeleihtesvideo']."</p></div>");
                         }
-                    ?>
-                </div>
-                <div class="Box-film">Film</div>
-                <div class="Box-kunde">Kunde</div>
-                <div class="Ausleihe-Status">Status</div>
-                
+                ?>
             </div>
-            
-            
-            
+
         </div>
         <div class="Erfassen">
             <div class="Ausleihe-übercontainer">
                 <div class="Ausleihe-Text">Ausleihen erfassen</div>
             </div>
-            
+    
             <div class="Ausleihe-Box2">
                 <form action="http://localhost/Projekt/_Framework/erfassen" method="POST">
                 
-                    <div class="rechts"><input type="text" name="name" class="inputfield" placeholder="Name" required></div>
-                    <div class="rechts"><input type="email" name="email" class="inputfield" placeholder="E-Mail" required></div>
-                    <div class="rechts"><input type="text" name="telefon" class="inputfield" placeholder="Telefon" required></div>
-                    <div class="rechts"><input type="text" name="film" class="inputfield" placeholder="Ausgeleihtes Video"></div>
-                    <div class="mehrerauswahlen"><select name="mitgliedschaft">
+                        <div class="rechts"><input name="name" type="text" class="inputfield" placeholder="Name" required></div>
+                        <div class="rechts"><input name="email" type="email" class="inputfield" placeholder="E-Mail" required></div>
+                        <div class="rechts"><input name="telefon" type="text" class="inputfield" placeholder="Telefon" ></div>
+                        <div class="rechts"><input name="film" type="text" class="inputfield" placeholder="Ausgeleihtes Video"required></div>
+                        <div class="mehrerauswahlen">Treue<select name="mitgliedschaft">
                             <option value="Gold">Gold</option>
                             <option value="Silver">Silver</option>
                             <option value="Bronze">Bronze</option>
                             <option value="keine">keine</option>
-                        </select></div>
+                          </select></div>
                         
                           <div class="rechts"><input type="submit" class="submitbutton" placeholder="Senden"></div>
                 
 
                 </form>
-                    
-        
-                
-                
-
-
-
 
             </div>
         </div>
-    </div>
-    
 
 
-
-
-
-
-
-
-
-
-
-
-
+    </div>    
 </body>
 </html>
